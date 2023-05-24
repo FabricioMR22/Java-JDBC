6c0591e629ed51222e63c2b3f629e01672c19810
Fabricio Martinez <u19218420@utp.edu.pe>
2023-05-24 18:05:27 -0500

    Modulo read and update

--- src/main/java/org/controller/ProductController.java
+++ src/main/java/org/controller/ProductController.java
@@ -30,16 +30,48 @@ public class ProductController {
         return 0;
     }
 
-    public void read() {
+    public Producto read(Integer ID) throws SQLException {
+        Connection con = new ConnectionFactory().recuperaConexion();
+        PreparedStatement statement = con.prepareStatement(
+                "SELECT * FROM producto WHERE id = ?;"
+        );
+        statement.setInt(1,ID);
+        statement.execute();
+
+        ResultSet resultSet = statement.getResultSet();
+
+        Producto producto = new Producto();
+        while (resultSet.next()){
+            producto.setId(resultSet.getInt("id"));
+            producto.setNombre(resultSet.getString("nombre"));
+            producto.setDescripcion(resultSet.getString("descripcion"));
+            producto.setCantidad(resultSet.getInt("cantidad"));
+        }
+        return producto;
     }
 
-    public void update() {
+    public Integer update(Producto producto) throws SQLException {
+        Connection con = new ConnectionFactory().recuperaConexion();
+        PreparedStatement statement = con.prepareStatement(
+                "UPDATE producto SET nombre = ?, descripcion = ?, cantidad = ? WHERE id = ?;"
+        );
+        statement.setString(1,producto.getNombre());
+        statement.setString(2,producto.getDescripcion());
+        statement.setInt(3,producto.getCantidad());
+        statement.setInt(4,producto.getId());
+
+        statement.execute();
+
+        int CountMod = statement.getUpdateCount();
+        con.close();
+
+        return CountMod;
     }
 
     public Integer delete(Integer ID) throws SQLException{
         Connection con = new ConnectionFactory().recuperaConexion();
         PreparedStatement statement = con.prepareStatement(
-                "DELETE FROM producto WHERE id = ?"
+                "DELETE FROM producto WHERE id = ?;"
         );
 
         statement.setInt(1,ID);
@@ -56,7 +88,7 @@ public class ProductController {
         List<Producto> Productos = new ArrayList<>();
 
         Statement statement = con.createStatement();
-        statement.execute("SELECT * FROM PRODUCTO");
+        statement.execute("SELECT * FROM producto;");
         ResultSet resultSet = statement.getResultSet();
 
         while (resultSet.next()) {
