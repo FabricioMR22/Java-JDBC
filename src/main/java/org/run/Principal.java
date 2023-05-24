30de0eece64117e30f34b7ab07136c6c0f497711
Fabricio Martinez <u19218420@utp.edu.pe>
2023-05-24 01:08:35 -0500

    Separando controller de main

---
+++ src/main/java/org/run/Principal.java
@@ -0,0 +1,28 @@
+package org.run;
+
+import org.controller.ProductController;
+import org.model.Producto;
+
+import java.sql.SQLException;
+
+public class Principal {
+    private static ProductController pro = new ProductController();
+
+    public static void main(String[] args) {
+        try {
+
+            pro.create(new Producto("Casaca","De cuero",30));
+            pro.listar().forEach(producto ->
+                    System.out.println( producto.getId() +" - "
+                            + producto.getNombre() + " - "
+                            + producto.getDescripcion() + " - "
+                            + producto.getCantidad()
+                    )
+            );
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
