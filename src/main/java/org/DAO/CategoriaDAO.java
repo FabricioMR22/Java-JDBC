00e41471787293c349e7fdc0f53509bd693a1959
Fabricio Martinez <u19218420@utp.edu.pe>
2023-05-25 23:26:35 -0500

    categoria DAO - modulo listar

--- src/main/java/org/DAO/CategoriaDAO.java
+++ src/main/java/org/DAO/CategoriaDAO.java
@@ -1,4 +1,41 @@
 package org.DAO;
 
+import org.model.Categoria;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoriaDAO {
+    private Connection con;
+
+    public CategoriaDAO(Connection con) {
+        this.con = con;
+    }
+
+    public List<Categoria> listar() {
+        List<Categoria> categorias = new ArrayList<>();
+
+        try{
+            final PreparedStatement statement = con.prepareStatement("SELECT * FROM categoria");
+            try(statement) {
+                statement.execute();
+                final ResultSet resultSet = statement.executeQuery();
+                try(resultSet) {
+                    while (resultSet.next()){
+                        Categoria categoria = new Categoria(
+                                resultSet.getInt("id"),
+                                resultSet.getString("nombre")
+                        );
+                        categorias.add(categoria);
+                    }
+                }
+            }
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+        return categorias;
+    }
 }
