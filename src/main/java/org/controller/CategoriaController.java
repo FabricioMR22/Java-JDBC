9444a54a41b41a88a3c1862753d48d4a850c6833
Fabricio Martinez <u19218420@utp.edu.pe>
2023-05-25 23:26:51 -0500

    categoria Controller - modulo listar

--- src/main/java/org/controller/CategoriaController.java
+++ src/main/java/org/controller/CategoriaController.java
@@ -1,4 +1,21 @@
 package org.controller;
 
+import factory.ConnectionFactory;
+import org.DAO.CategoriaDAO;
+import org.model.Categoria;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoriaController {
+    private CategoriaDAO categoriaDAO;
+
+    public CategoriaController() {
+        this.categoriaDAO = new CategoriaDAO(new ConnectionFactory().recuperaConexion());
+    }
+
+    public List<Categoria> listar(){
+        return categoriaDAO.listar() ;
+    }
+
 }
