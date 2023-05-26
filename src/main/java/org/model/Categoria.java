1e40e3510ab39357a431d808e1e55bc2296af13b
Fabricio Martinez <u19218420@utp.edu.pe>
2023-05-25 23:27:05 -0500

    categoria Model - modulo listar

--- src/main/java/org/model/Categoria.java
+++ src/main/java/org/model/Categoria.java
@@ -8,4 +8,28 @@ public class Categoria {
         this.id = id;
         this.nombre = nombre;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
 }
