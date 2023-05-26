2c2df5cfd25e80f58ecac43b6cec1d950b35cfa6
Fabricio Martinez <u19218420@utp.edu.pe>
2023-05-25 23:27:24 -0500

    uso de modulo categoria

--- src/main/java/org/run/Principal.java
+++ src/main/java/org/run/Principal.java
@@ -1,25 +1,20 @@
 package org.run;
 
+import org.controller.CategoriaController;
 import org.controller.ProductController;
 import org.model.Producto;
 
 
 public class Principal {
-    private static ProductController controller = new ProductController();
+
 
     public static void main(String[] args) {
 
-            //controller.guardar(new Producto("Linternas", "Pilas recargable",60));
+        CategoriaController controller = new CategoriaController();
 
-            //System.out.println(controller.buscar(22));
-
-            controller.listar().forEach(producto ->
-                    System.out.println( producto.getId() +" - "
-                            + producto.getNombre() + " - "
-                            + producto.getDescripcion() + " - "
-                            + producto.getCantidad()
-                    )
-            );
+        controller.listar().forEach(categoria ->
+                System.out.println( categoria)
+        );
 
     }
 
