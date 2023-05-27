bdb94d79a3bc55537bf904a871ea707145c61a20
Fabricio Martinez <u19218420@utp.edu.pe>
2023-05-26 23:04:04 -0500

    cargar reportes con una sola conexion

---
+++ src/main/java/org/run/runCateProdu.java
@@ -0,0 +1,23 @@
+package org.run;
+
+import org.controller.CategoriaController;
+
+public class runCateProdu {
+    public static void main(String[] args) {
+        CategoriaController categoriaController = new CategoriaController();
+
+        categoriaController.cargaReporte().forEach(
+                categoria -> {
+                    System.out.printf("%-15s%n", categoria.getNombre());
+                    categoria.mostrarProducto().forEach(
+                            producto -> System.out.printf("%-15s %-20s %-5s%n",
+                                    " ",
+                                    producto.getNombre(),
+                                    producto.getCantidad())
+                    );
+                }
+        );
+
+
+    }
+}
