e236e4d6ed1094ebc8c717ec64fa561aed21aaaa
Fabricio Martinez <u19218420@utp.edu.pe>
2023-05-28 22:55:56 -0500

    metodo delete, usando el ciclo de merge para poder removerlo

--- src/main/java/JPA/dao/CategoriaDAO.java
+++ src/main/java/JPA/dao/CategoriaDAO.java
@@ -15,7 +15,12 @@ public class CategoriaDAO {
         this.em.persist(categoria);
     }
 
-    public void actualizar(Categoria categoria){
+    public void update(Categoria categoria){
         this.em.merge(categoria);
+    }
+
+    public void delete(Categoria categoria){
+        categoria = em.merge(categoria);
+        this.em.remove(categoria);
     }
 }
