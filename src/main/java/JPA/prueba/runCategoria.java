1e251f20e236199328fa05ce630deb3a28085ebf
Fabricio Martinez <u19218420@utp.edu.pe>
2023-05-28 22:55:28 -0500

    utilizando find, que manda el objeto a managed, y luego actualizando con flush

---
+++ src/main/java/JPA/prueba/runCategoria.java
@@ -0,0 +1,24 @@
+package JPA.prueba;
+
+import JPA.dao.CategoriaDAO;
+import JPA.model.Categoria;
+import JPA.utils.JPAUtils;
+
+import javax.persistence.EntityManager;
+
+public class runCategoria {
+    public static void main(String[] args) {
+        EntityManager manager = JPAUtils.getEntityManager();
+
+        CategoriaDAO categoriaDAO = new CategoriaDAO(manager);
+
+        Categoria celulares = new Categoria("Celulares");
+
+        manager.getTransaction().begin();
+        categoriaDAO.save(celulares);
+
+        Categoria categoria = manager.find(Categoria.class,1l);
+        manager.remove(categoria);
+        manager.flush();
+    }
+}
