1d00a0b17bbd4b73598f73904f28d29db710a503
Fabricio Martinez <u19218420@utp.edu.pe>
2023-05-29 00:21:06 -0500

    metodo lista con dos parametros en query

--- src/main/java/JPA/dao/ProductoDAO.java
+++ src/main/java/JPA/dao/ProductoDAO.java
@@ -2,7 +2,9 @@ package JPA.dao;
 
 import JPA.model.Producto;
 
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,5 +26,14 @@ public class ProductoDAO {
     public List<Producto> read(){
         String query = "SELECT P FROM Producto AS P";
         return em.createQuery(query,Producto.class).getResultList();
+    }
+
+    public List<Producto> read(String nombre,String descripcion){
+        String query = "SELECT P FROM Producto AS P WHERE P.nombre =: nombre OR P.descripcion=:descripcion ";
+        Query querySelect = em.createQuery(query);
+        querySelect.setParameter("nombre",nombre);
+        querySelect.setParameter("descripcion",descripcion);
+
+        return querySelect.getResultList();
     }
 }
