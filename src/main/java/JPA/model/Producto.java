64809dd52dbcf4e921dc8e9de867c1147a1a186d
Fabricio Martinez <u19218420@utp.edu.pe>
2023-05-28 19:33:09 -0500

    update model producto

--- src/main/java/JPA/model/Producto.java
+++ src/main/java/JPA/model/Producto.java
@@ -1,6 +1,8 @@
 package JPA.model;
 
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -8,18 +10,26 @@ import java.math.BigDecimal;
 
 @Entity
 @Table(name = "productos")
+@RequiredArgsConstructor
 public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter @Setter
     private Long id;
 
-    @Column @Getter @Setter
+    @Column
+    @Getter @Setter @NonNull
     private String nombre;
 
-    @Column @Getter @Setter
+    @Column
+    @Getter @Setter @NonNull
     private String descripcion;
 
-    @Column @Getter @Setter
+    @Column
+    @Getter @Setter @NonNull
     private BigDecimal precio;
+
+    public Producto() {
+
+    }
 }
